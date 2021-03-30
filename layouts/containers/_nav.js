8574e2da242b4d5cdf7eae9d1692620367a4b3a1
congdp <congdinhpham99@gmail.com>
2021-03-30 18:25:09 +0700

    update

--- layouts/containers/_nav.js
+++ layouts/containers/_nav.js
@@ -1,12 +1,11 @@
 export default [{
     _name: 'CSidebarNav',
     _children: [{
-        _name: 'CSidebarNavItem',
-        name: 'Home',
-        to: '/dashboard',
-        icon: 'cil-speedometer',
+            _name: 'CSidebarNavItem',
+            name: 'Home',
+            to: '/dashboard',
+            icon: 'cil-speedometer',
 
-<<<<<<< HEAD
         },
         {
             _name: 'CSidebarNavItem',
@@ -14,7 +13,6 @@ export default [{
             to: '/project',
             icon: 'cil-drop'
         },
-
         {
             _name: 'CSidebarNavItem',
             name: 'Task',
@@ -28,33 +26,5 @@ export default [{
             to: '/user',
             icon: 'cil-pencil'
         },
-        {
-            _name: 'CSidebarNavItem',
-            name: 'Logout',
-            to: '/login',
-            icon: 'cil-pencil'
-        },
-=======
-    },
-    {
-        _name: 'CSidebarNavItem',
-        name: 'Project',
-        to: '/project',
-        icon: 'cil-drop'
-    },
-    {
-        _name: 'CSidebarNavItem',
-        name: 'Task',
-        to: '/task',
-        icon: 'cil-drop'
-    },
->>>>>>> ad45771b5cfc12d4c28b3788b0a466128ac074af
-
-    {
-        _name: 'CSidebarNavItem',
-        name: 'User',
-        to: '/user',
-        icon: 'cil-pencil'
-    },
     ]
 }]
