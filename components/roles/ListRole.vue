cc558e65935bced575cf9847b0f5e751e425e2fd
khangkhoai <khangnguyen2311998@gmail.com>
2021-04-01 09:58:03 +0700

    khang

--- components/roles/ListRole.vue
+++ components/roles/ListRole.vue
@@ -1,46 +1,84 @@
 <template>
-  <CCardBody >
-    <CButton color="info">
-      <nuxt-link :to="`/roles/Create`">Add</nuxt-link>
-    </CButton>
-    <CDataTable :items="listRoles"
-      :fields="fields"
-      items-per-page-select
-      :items-per-page="5"
-      hover
-      sorter
-      pagination>
-      <template #show_details="{item}">
-        <td class="py-2">
-          <CButton size="sm-6" color="danger" @click="deleteRole(item.id)">
-                Delete
-          </CButton>
-        </td>
-      </template> 
-    </CDataTable>
-  </CCardBody>
+  <div>
+    <CCard>
+      <CCardHeader>
+        <h3>List Role</h3>
+      </CCardHeader>
+      <CCardBody >
+        <CButton color="primary" class="m-2 btn_add">
+          <nuxt-link to="/roles/Create" class="text-white d-block"> + Add</nuxt-link>
+        </CButton>
+      <CDataTable :items="listRoles"
+          :fields="fields"
+          items-per-page-select
+          :items-per-page="5"
+          hover
+          pagination
+        >
+        <template #show_details="{item}">
+          <CButtonGroup>
+            <CButton  color="danger" @click="deleteRole(item.id)">
+                  Delete
+            </CButton>
+            <CButton color="success">
+              <nuxt-link :to="`/roles/${item.id}`">Edit</nuxt-link>
+            </CButton>
+          </CButtonGroup>  
+        </template> 
+      </CDataTable>
+      </CCardBody>
+    </CCard>
+  </div>  
 </template>
-
 <script>
-
 import axios from 'axios'
+const fields = [
+    { key: 'id', label: 'ID',_style:'min-width:50px' },
+    { key: 'name', label: 'RoleName',_style:'min-width:150px' },
+    { key: 'created_at', _style:'min-width:50px;' },
+    { key: 'updated_at', _style:'min-width:50px;' },
+    { 
+    key: 'show_details', 
+    label: '', 
+    _style: 'width:1%', 
+    sorter: false, 
+    filter: false
+    }
+]
 export default {
   name: 'ListRole',
+   props: {
+    listRoles: {
+      type: Array,
+      default: () => [],
+    },
+  },
   data () {
     return {
+      fields : fields ,
+      keySearch : ''
       
-      fields,
-      details: [],
-      collapseDuration: 0,
-      page : {},
-      currentPage : 1
     }
   },
   methods: {
    
-    
+    deleteRole(id)
+    {
+      console.log(id)
+      axios.delete('http://127.0.0.1:8000/api/roles/' + id).then(response => {
+        this.items.splice((index), 1)
+        });
+    },
+    searchRole(value){
+      this.keySearch=value;
+      console.log(this.keySearch)
+      axios.get("http://localhost:8000/api/roles/search?name="+this.keySearch).then(res => {
+        this.dataUser = res.data;
+       
+      });
+    }
   },
-   
+  
 }
 </script>
 
--- pages/roles/index.vue
+++ pages/roles/index.vue
@@ -1,64 +1,19 @@
 <template>
   <div>
-    <CCard>
-      <CCardBody >
-        <CButton color="info">
-          <nuxt-link :to="`/roles/Create`">Add</nuxt-link>
-        </CButton><br>
-        <search-form @getKeySearch=searchRole /><br>
-      <CDataTable :items="listRoles"
-                  :fields="fields"
-                  hover
-                  sorter
-        pagination>
-        <template #show_details="{item}">
-          <CButtonGroup>
-            <CButton  color="danger" @click="deleteRole(item.id)">
-                  Delete
-            </CButton>
-            <CButton color="success">
-              <nuxt-link :to="`/roles/${item.id}`">Edit</nuxt-link>
-            </CButton>
-          </CButtonGroup>  
-        </template> 
-      </CDataTable>
-      </CCardBody>
-    </CCard>
+    <list-role :listRoles="listRoles"></list-role>
   </div>  
 </template>
 <script>
-const fields = [
-    { key: 'id', label: 'ID',_style:'min-width:50px' },
-    { key: 'name', label: 'RoleName',_style:'min-width:150px' },
-    { key: 'created_at', _style:'min-width:50px;' },
-    { key: 'updated_at', _style:'min-width:50px;' },
-    { 
-    key: 'show_details', 
-    label: '', 
-    _style: 'width:1%', 
-    sorter: false, 
-    filter: false
-    }
-]
 import ListRole from "@/components/roles/ListRole.vue";
-import SearchForm from "@/components/roles/SearchForm.vue";
+
 import axios from 'axios'
 export default {
-  name: 'ListRole',
   components : {
-    ListRole,
-    SearchForm
+    ListRole
   },
   data () {
     return {
       listRoles:[],
-      fields,
-      details: [],
-      collapseDuration: 0,
-      page : {},
-      currentPage : 1,
-      keySearch : ''
-      
     }
   },
   methods: {
@@ -66,27 +21,11 @@ export default {
       axios({method: 'GET',url: 'http://127.0.0.1:8000/api/roles/',data: null}).then(res =>{this.listRoles = res.data; 
       }).catch(err => {console.log(err)}) 
     }, 
-    deleteRole(id)
-    {
-      console.log(id)
-      axios.delete('http://127.0.0.1:8000/api/roles/' + id).then(response => {
-        this.items.splice((index), 1)
-        });
-    },
-    searchRole(value){
-      this.keySearch=value;
-      console.log(this.keySearch)
-      axios.get("http://localhost:8000/api/roles/search?name="+this.keySearch).then(res => {
-        this.dataUser = res.data;
-       
-      });
-    }
   },
   mounted () {
     this.listData();   
   },
 }
 </script>
-
 <style>
 </style>
