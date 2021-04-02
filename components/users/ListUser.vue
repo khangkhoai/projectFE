61e67512f2311607ef40bdc9d4ba28494635bd43
khangkhoai <khangnguyen2311998@gmail.com>
2021-04-02 18:19:01 +0700

    update khang

--- components/users/ListUser.vue
+++ components/users/ListUser.vue
@@ -22,12 +22,54 @@
               {{getRoleName(item.role_id)}}
             </td>
         </template>
+         <template #show_details="{ item, index }">
+            <td class="py-2">
+              <CButton
+                color="primary"
+                variant="outline"
+                square
+                size="sm"
+                @click="toggleDetails(item, index),getTaskOfUser(item.id)"
+              >
+                {{ Boolean(item._toggled) ? "Hide" : "Show" }}
+              </CButton>
+            </td>
+          </template>
+           <template #details="{ item }">
+            <CCollapse
+              :show="Boolean(item._toggled)"
+              :duration="collapseDuration"
+            >
+              <CCardBody>
+                <h6>List Task</h6>
+                <CDataTable 
+                :items="listTasks"
+                :fields="user_task"
+                hover
+                pagination
+                striped
+                >
+                </CDataTable>
+              </CCardBody>
+            </CCollapse>
+          </template>
         </CDataTable>
       </CCardBody>
     </CCard>
   </div>
 </template>
 <script>
+const user_task = [
+  { key: "id", label: "Id" },
+  { key: "subject", label: "Name" },
+  { key: "id_project", label: "Project" },
+  { key: "content", label: "Content" },
+  { key: "member_id", label: "Assignee" },
+  { key: "status", label: "Status" },
+  { key: "start_date", label: "Start Date" },
+  { key: "due_date", label: "Due Date" },
+ 
+];
 import axios from "axios";
 export default {
   name: "ListUser",
@@ -43,8 +85,14 @@ export default {
   },
   data() {
     return {
+      dataUser: this.dataUser.map((item, id) => {
+        return { item, id };
+      }),
       dataRole: [],
-      data: {}
+      listTasks : [],
+      details: [],
+      collapseDuration: 0,
+      user_task : user_task
     };
   },
   methods: {
@@ -57,7 +105,6 @@ export default {
         // console.log(this.dataRole);
       });
     },
-
     /**
      * get name of role 
      * @param Interger role_id of data user
@@ -65,10 +112,26 @@ export default {
      */
     getRoleName(id){
      return this.dataRole.filter(role =>{return role.id==id}).map(role => {return role.name}).join(',');
+    },
+    toggleDetails(item, index) {
+      this.$set(this.dataUser[index], "_toggled", !item._toggled);
+      this.collapseDuration = 300;
+      this.$nextTick(() => {
+        this.collapseDuration = 0;
+      });
+    },
+    getTaskOfUser(id){
+       axios.get("http://localhost:8000/api/user/get/"+id).then(res => {
+        this.listTasks = res.data;
+        console.log(res.data);
+        // this.page = res.data;
+      });
+      
     }
   },
   mounted(){
     this.getRole();
+    
   }
 };
 </script>
